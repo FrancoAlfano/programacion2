f12beafa256dd679428e62c2983ae671939780c2
FrancoAlfano <franco.alfano.95@gmail.com>
2019-05-29 16:56:02 -0300

    adding databasecon

---
+++ 2019/databaseCon/src/main/java/Practica_tp1/databaseCon/App.java
@@ -0,0 +1,13 @@
+package Practica_tp1.databaseCon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
